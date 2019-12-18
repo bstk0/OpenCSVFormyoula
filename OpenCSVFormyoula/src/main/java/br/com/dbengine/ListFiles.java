f358ab613f3aa6a9b71e162370252c98f864f203
rodrigo.bisterco <rodrigo.bisterco@gmail.com>
2019-12-18 18:22:59 -0300

    funcionou o list

---
+++ OpenCSVFormyoula/src/main/java/br/com/dbengine/ListFiles.java
@@ -0,0 +1,42 @@
+package br.com.dbengine;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListFiles {
+
+	public static void main(String[] args) {
+
+		// final File folder = new File("C:\\projects");
+		// final File folder = new File("E:/OneDrive - Infosys
+		// Limited/Syngenta/Formyoula/Complaints");
+		final File folder = new File("C://Users/rodrigo.bisterco/Downloads");
+
+		List<String> result = new ArrayList<>();
+
+		search(".*\\.csv", folder, result);
+
+		for (String s : result) {
+			System.out.println(s);
+		}
+
+	}
+
+	public static void search(final String pattern, final File folder, List<String> result) {
+		for (final File f : folder.listFiles()) {
+
+			if (f.isDirectory()) {
+				search(pattern, f, result);
+			}
+
+			if (f.isFile()) {
+				if (f.getName().matches(pattern)) {
+					result.add(f.getAbsolutePath());
+				}
+			}
+
+		}
+	}
+
+}
