4b4d1aeae93cbb5acec170d43932c13aa7bff23b
rodrigo.bisterco <rodrigo.bisterco@gmail.com>
2019-12-20 16:25:06 -0300

    full ok

--- OpenCSVFormyoula/src/main/java/br/com/dbengine/CSVtoSheet2.java
+++ OpenCSVFormyoula/src/main/java/br/com/dbengine/CSVtoSheet2.java
@@ -5,9 +5,21 @@ import java.io.IOException;
 class CSVtoSheet2 {
     
 	final static String FILE_PATH = "E://OneDrive - Infosys Limited/Syngenta/Formyoula/Complaints/Report_Auditoria/";
-	final static String FILE_SEARCH = "Formulário de Ocorrências - Logistica";
 	final static String EXCEL_NAME = "workbook2.xls";
-	final static String SHEET_NAME = "Logistica";
+	//final static String FILE_SEARCH = "Formulário de Ocorrências - Logistica";
+	//final static String SHEET_NAME = "Logistica";
+	//
+	//final static String FILE_SEARCH = "Formulário de Ocorrências - Reclamação de Produto";
+	//final static String SHEET_NAME = "Reclamacao Produto";
+	//
+	//final static String FILE_SEARCH = "Formulário de Ocorrências - Erros na NF";
+	//final static String SHEET_NAME = "Erros na NF";
+	//
+	//final static String FILE_SEARCH = "Formulário de Ocorrências - Serviços Adicionais";
+	//final static String SHEET_NAME = "Serviços Adicionais";
+	//
+	final static String FILE_SEARCH = "Formulário de Ocorrências - Acordo Comercial";
+	final static String SHEET_NAME = "Acordo Comercial";
 	
 
 	public static void main(String[] args) throws IOException {
