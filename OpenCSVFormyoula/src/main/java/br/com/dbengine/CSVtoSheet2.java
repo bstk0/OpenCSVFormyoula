536b738a25fb70be1915d024b52b781e82a2c9de
rodrigo.bisterco <rodrigo.bisterco@gmail.com>
2019-12-20 15:12:38 -0300

    v1

--- OpenCSVFormyoula/src/main/java/br/com/dbengine/CSVtoSheet2.java
+++ OpenCSVFormyoula/src/main/java/br/com/dbengine/CSVtoSheet2.java
@@ -1,29 +1,13 @@
 package br.com.dbengine;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.CreationHelper;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-
-import com.opencsv.CSVReader;
 
 class CSVtoSheet2 {
     
+	final static String FILE_PATH = "E://OneDrive - Infosys Limited/Syngenta/Formyoula/Complaints/Report_Auditoria/";
 	final static String FILE_SEARCH = "Formulário de Ocorrências - Logistica";
 	final static String EXCEL_NAME = "workbook2.xls";
 	final static String SHEET_NAME = "Logistica";
-	final static String FILE_PATH = "E://OneDrive - Infosys Limited/Syngenta/Formyoula/Complaints/Report_Auditoria/";
 	
 
 	public static void main(String[] args) throws IOException {
@@ -32,117 +16,8 @@ class CSVtoSheet2 {
     	//final File file = FILE_PATH + fileName; //"/tmp/sheet.xls";
 		
 		CSVtoSheet2Impl csv2 = new CSVtoSheet2Impl(); 
+		csv2.generateExcel(FILE_PATH, FILE_SEARCH, EXCEL_NAME,SHEET_NAME);
 		
-    	File file = new File(FILE_PATH + EXCEL_NAME);
-    	
-    	final HSSFWorkbook wb; //workbook;
-    	if (file.exists() == false) {
-    	  System.out.println("Creating a new workbook '" + file + "'");
-    	  wb = new HSSFWorkbook();
-    	  csv2.setWriteReader(false);
-    	  
-    	} else {
-    	  System.out.println("Appending to existing workbook '" + file + "'");
-    	  final InputStream is = new FileInputStream(file);
-    	  try {
-    		  wb = new HSSFWorkbook(is);
-    	  } finally {
-    	    is.close();
-    	  }
-    	}
-    	
-    	//listing Files
-		final File folder = new File(FILE_PATH);
-    	
-    	List<String> result = new ArrayList<>();
-    	
-    	ListFiles.search(FILE_SEARCH+".*\\.csv", folder, result);
-    	
-    	HSSFSheet sheet = getSheet(wb);
-
-		for (String s : result) {
-			System.out.println(s);
-			loadWriteSheet(FILE_PATH, SHEET_NAME, s, sheet);
-		}
-       
-
-        /*
-        // test to see if a file exists
-        File file = new File(FILE_PATH + "workbook.xls");
-        //exists = file.exists();
-        if (file.exists() && file.isFile())
-        {
-          System.out.println("file exists, and it is a file");
-        }
-        */
-        
-        // Write the output to a file
-        FileOutputStream fileOut = new FileOutputStream(FILE_PATH + EXCEL_NAME);
-        wb.write(fileOut);
-        fileOut.close();
-    }
-
-	private static void loadWriteSheet(final String FILE_PATH, final String SHEET_NAME,String fileName, HSSFSheet sheet)
-			throws FileNotFoundException, IOException {
-		//Workbook wb = new HSSFWorkbook();
-        CreationHelper helper = sheet.getWorkbook().getCreationHelper();
-
-        
-        /*
-        System.out.println("wb.getNumberOfSheets():" + wb.getNumberOfSheets());
-		if (wb.getNumberOfSheets() != 0) {
-	        for (int i = 0; i < wb.getNumberOfSheets(); i++) {
-	          System.out.println("wb.getSheetName(i)" + wb.getSheetName(i));	
-	           if (wb.getSheetName(i).equals(SHEET_NAME)) {
-	        	    System.out.println("Achou");
-	                sheet = wb.getSheet(SHEET_NAME);
-	                sheet.shiftRows(1, sheet.getLastRowNum()+1, 1, true,true);
-	            } else {
-	            	System.out.println("Não Achou");
-	            	sheet = wb.createSheet(SHEET_NAME);
-	            }
-	        }
-	    }
-	    else {
-	        // Create new sheet to the workbook if empty
-	        sheet = wb.createSheet(SHEET_NAME);
-	    }
-		*/
-		int rowCount = sheet.getLastRowNum()+1;
-
-        @SuppressWarnings("resource")
-		CSVReader reader = new CSVReader(new FileReader(FILE_PATH + fileName));
-        String[] line;
-        //int r = 0;
-        while ((line = reader.readNext()) != null) {
-            Row row = sheet.createRow((short) rowCount++);
-			//System.out.println("sheet.getLastRowNum():" + sheet.getLastRowNum());
-			//sheet.shiftRows(1, sheet.getLastRowNum()+1, 1, true,true);
-            
-            //TO DO: HEADER SÓ PARA O PRIMEIRO ARQUIVO ...
-            for (int i = 0; i < line.length; i++)
-                row.createCell(i)
-                   .setCellValue(helper.createRichTextString(line[i]));
-        }
-        //reader.close();
-	}
-	
-	
-	private static HSSFSheet getSheet(final HSSFWorkbook wb) {
-       
-        //Sheet sheet = null;
-        HSSFSheet sheet = wb.getSheet(SHEET_NAME);
-		if(sheet == null){
-			//throw new Exception("Sheet doesnot exist...");
-			System.out.println("Sheet doesnot exist...");
-			sheet = wb.createSheet(SHEET_NAME);
-		}
-		else {
-			System.out.println("sheet.getLastRowNum():" + sheet.getLastRowNum());
-			//sheet.shiftRows(1, sheet.getLastRowNum()+1, 1, true,true);
-		}
-		return sheet;
-	}
-	
+    }	
 	
 }
--- OpenCSVFormyoula/src/main/java/br/com/dbengine/CSVtoSheet2Impl.java
+++ OpenCSVFormyoula/src/main/java/br/com/dbengine/CSVtoSheet2Impl.java
@@ -1,15 +1,144 @@
 package br.com.dbengine;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Row;
+
+import com.opencsv.CSVReader;
+
 public class CSVtoSheet2Impl {
 
-	boolean writeReader;
+	boolean jumpHeader;
 	
-	public boolean isWriteReader() {
-		return writeReader;
+	public void generateExcel(String FILE_PATH, String FILE_SEARCH, String EXCEL_NAME,String SHEET_NAME) {
+		HSSFWorkbook wb = null; //workbook;
+		File file = new File(FILE_PATH + EXCEL_NAME);
+
+		if (file.exists() == false) {
+			System.out.println("Creating a new workbook '" + file + "'");
+			wb = new HSSFWorkbook();
+		} else {
+			try {
+				System.out.println("Appending to existing workbook '" + file + "'");
+				final InputStream is = new FileInputStream(file);
+				wb = new HSSFWorkbook(is);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				// is.close();
+			}
+		}
+    	
+    	//listing Files
+		final File folder = new File(FILE_PATH);
+    	
+    	List<String> result = new ArrayList<>();
+    	
+    	ListFiles.search(FILE_SEARCH+".*\\.csv", folder, result);
+    	
+    	HSSFSheet sheet = getSheet(wb,SHEET_NAME);
+
+		for (String s : result) {
+			System.out.println(s);
+			try {
+				loadWriteSheet(FILE_PATH, SHEET_NAME, s, sheet);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+       
+      
+        // Write the output to a file
+        FileOutputStream fileOut;
+		try {
+			fileOut = new FileOutputStream(FILE_PATH + EXCEL_NAME);
+			wb.write(fileOut);
+	        fileOut.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	        
+		
+	}
+	
+	private void loadWriteSheet(final String FILE_PATH, final String SHEET_NAME,String fileName, HSSFSheet sheet)
+			throws FileNotFoundException, IOException {
+		//Workbook wb = new HSSFWorkbook();
+        CreationHelper helper = sheet.getWorkbook().getCreationHelper();
+        int rowCount = 0;
+        if (sheet.getLastRowNum() == 0) {
+        	setJumpHeader(false);
+        } else {
+        	setJumpHeader(true);
+        	rowCount = sheet.getLastRowNum()+1;
+        }
+
+        @SuppressWarnings("resource")
+		CSVReader reader = new CSVReader(new FileReader(FILE_PATH + fileName));
+        String[] line;
+        //int r = 0;
+        while ((line = reader.readNext()) != null) {
+            if (isJumpHeader()) {
+            	setJumpHeader(false);
+            	continue;
+            }
+        	//line - rows
+            Row row = sheet.createRow((short) rowCount++);
+            //colunas
+            for (int i = 0; i < line.length; i++) {
+                row.createCell(i)
+                .setCellValue(helper.createRichTextString(line[i]));
+            }
+        }
+        //reader.close();
+	}
+	
+	private HSSFSheet getSheet(final HSSFWorkbook wb, String SHEET_NAME) {
+	       
+        //Sheet sheet = null;
+        HSSFSheet sheet = wb.getSheet(SHEET_NAME);
+		if(sheet == null){
+			//throw new Exception("Sheet doesnot exist...");
+			System.out.println("Sheet doesnot exist...");
+			sheet = wb.createSheet(SHEET_NAME);
+		}
+		else {
+			System.out.println("sheet.getLastRowNum():" + sheet.getLastRowNum());
+			//sheet.shiftRows(1, sheet.getLastRowNum()+1, 1, true,true);
+		}
+		return sheet;
+	}
+	
+	public boolean isJumpHeader() {
+		return jumpHeader;
 	}
 
-	public void setWriteReader(boolean writeReader) {
-		this.writeReader = writeReader;
+	public void setJumpHeader(boolean jumpHeader) {
+		this.jumpHeader = jumpHeader;
 	}
+
 
 }
